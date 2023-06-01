64087e8b3cd9a8eac2d764b69ec4cc3a2be8307c
Scott <109820423+8cott@users.noreply.github.com>
2023-06-01 16:36:06 -0400

    added sass

--- views/layouts/navbar.jsx
+++ views/layouts/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     return (
        <>
         <header>
-    <div className="navbar">
+    <div className="navbar stacked-nav">
       <div className="logo"><a href="/listings"><img src="https://i.imgur.com/BHcMimH.png" alt="logo" className="srlogo" /></a></div>
       <ul className="links">
         <li><span className='title'>Real Estate Listings</span></li>
